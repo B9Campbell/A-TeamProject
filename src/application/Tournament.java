94bfb8644ae7367efa044e74e4135f11f3a76023
Evan Scott <escott1367@gmail.com>
2018-04-23 18:44:32 -0500

    Update Tournament.java

--- src/application/Tournament.java
+++ src/application/Tournament.java
@@ -17,14 +17,14 @@ public class Tournament {
         bracket.add(new Match[1]);
         
         //hard coding team names
-        bracket.get(0)[0] = new Match("Challenger 1 -", "Challenger 16-");
-        bracket.get(0)[1] = new Match("Challenger 8 -", "Challenger 9 -");
-        bracket.get(0)[2] = new Match("Challenger 4 -", "Challenger 13-");
-        bracket.get(0)[3] = new Match("Challenger 5 -", "Challenger 12-");
-        bracket.get(0)[4] = new Match("Challenger 2 -", "Challenger 15-");
-        bracket.get(0)[5] = new Match("Challenger 7 -", "Challenger 10-");
-        bracket.get(0)[6] = new Match("Challenger 3 -", "Challenger 14-");
-        bracket.get(0)[7] = new Match("Challenger 6 -", "Challenger 11-");
+        bracket.get(0)[0] = new Match("Challenger 1  -", "Challenger 16-");
+        bracket.get(0)[1] = new Match("Challenger 8  -", "Challenger 9  -");
+        bracket.get(0)[2] = new Match("Challenger 4  -", "Challenger 13-");
+        bracket.get(0)[3] = new Match("Challenger 5  -", "Challenger 12-");
+        bracket.get(0)[4] = new Match("Challenger 2  -", "Challenger 15-");
+        bracket.get(0)[5] = new Match("Challenger 7  -", "Challenger 10-");
+        bracket.get(0)[6] = new Match("Challenger 3  -", "Challenger 14-");
+        bracket.get(0)[7] = new Match("Challenger 6  -", "Challenger 11-");
         
     }
     
