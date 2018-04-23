582663174d7d700566d11ece32f8517015a37579
Evan Scott <escott1367@gmail.com>
2018-04-23 18:42:41 -0500

    Update TournamentGUI.java

--- src/application/TournamentGUI.java
+++ src/application/TournamentGUI.java
@@ -13,6 +13,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 
 public class TournamentGUI extends Application {
