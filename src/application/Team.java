b0d5db9c7ef509627487caeeda2758bb552bb670
Evan Scott <escott1367@gmail.com>
2018-04-23 17:48:59 -0500

    Create Team.java

---
+++ src/application/Team.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Team {
+	
+	private String name;
+	private int rank;
+	
+	public Team(){
+	}
+	
+	public Team(String name, int rank){
+		this.name = name;
+		this.rank = rank;
+	}
+
+}
