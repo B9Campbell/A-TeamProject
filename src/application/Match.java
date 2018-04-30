25e1de24621d9a2ce59f03a31401ced79ea5baa6
CertainlyCanyon <Canyonman133@gmail.com>
2018-04-30 15:34:05 -0500

    Fixed final alert and allowed for third place tie. Commented Match.java

--- src/application/Match.java
+++ src/application/Match.java
@@ -10,38 +10,42 @@ import javafx.scene.layout.Priority;
 
 public class Match {
     
-    private Team team1;
+    private Team team1; //the two teams in this match
     private Team team2;
     
-    private Button submitButton;
+    private Button submitButton; //button to submit scores
     
-    private Alert alert;
+    private Alert alert; //alert to show warnings
     
-    private Alert winners;
+    private Alert winners; //alert to show winners
     
     
-    
+    /**
+     * Creates a match with TBD teams and sets functionality for the submit button
+     */
     public Match()
     {
-        team1 = new Team();
+        team1 = new Team(); //create TBD teams with no-arg constructor
         team2 = new Team();
         
         submitButton = new Button("Submit Scores");
         
-        alert = new Alert(AlertType.WARNING);
+        alert = new Alert(AlertType.WARNING); //set alert types for alerts while instantiating them
         winners = new Alert(AlertType.INFORMATION);
         
-        submitButton.setOnAction(e -> {
+        submitButton.setOnAction(e -> { //when submit scores is pressed do this
             
             try
             {
                 if(team1.getName().trim().toUpperCase().equals("TBD") || team2.getName().trim().toUpperCase().equals("TBD"))
                 {
+                    //displays warning if the button is pressed before teams for this match have been determined
                     alert.setTitle("Warning");
                     alert.setHeaderText("Warning, one or more teams in this match has yet to be determined!");
                     alert.setContentText("Please complete scores for the previous games and click the \"Submit Scores\" button.");
                     alert.showAndWait();
                 } else if (Integer.parseInt(team1.getScoreField().getText().trim()) == Integer.parseInt(team2.getScoreField().getText().trim())) {
+                    //displays warning if the scored entered result in a tie
                     alert.setTitle("Warning");
                     alert.setHeaderText("Warning, this match cannot be a tie!");
                     alert.setContentText("Please rectify the issue and click the \"Submit Scores\" button.");
@@ -51,11 +55,24 @@ public class Match {
                         
                     if(Integer.parseInt(team1.getScoreField().getText().trim()) > Integer.parseInt(team2.getScoreField().getText().trim()))
                     {
+                      //this if-statement triggers if the submit scores button was pushed on the final match otherwise it updates the team in the next match
                         if(!TournamentGUI.tournament.setNextTeam(this, team1.getName(), team2))
                         {
+                            String third = ""; //get third place team based on scores of runner ups
+                            if(Integer.parseInt(team1.getRunnerUp().getScoreField().getText()) > (Integer.parseInt(team2.getRunnerUp().getScoreField().getText())))
+                            {
+                                third = team1.getRunnerUp().getName();
+                            } else if(Integer.parseInt(team1.getRunnerUp().getScoreField().getText()) < (Integer.parseInt(team2.getRunnerUp().getScoreField().getText())))
+                            {
+                                third = team2.getRunnerUp().getName();
+                            } else {//it is possible to tie here so we display both teams
+                                third = team1.getRunnerUp().getName() + " tied with " + team2.getRunnerUp().getName();
+                            }
+                            
+                            //show alert for winners
                             alert.setTitle("Tournament Over");
                             alert.setHeaderText("We have a winner!");
-                            alert.setContentText("First Place: " + team1.getName() + "\nSecond Place: " + team2.getName() + "\nThirdPlace: ");
+                            alert.setContentText("First Place: " + team1.getName() + "\nSecond Place: " + team2.getName() + "\nThirdPlace: " + third);
                             alert.showAndWait();
                         }
                         
@@ -65,19 +82,34 @@ public class Match {
                     {
                         if(!TournamentGUI.tournament.setNextTeam(this, team2.getName(), team1))
                         {
-                            String third = (Integer.parseInt(team1.getRunnerUp().getScoreField().getText()) > (Integer.parseInt(team2.getRunnerUp().getScoreField().getText()))) ? team1.getRunnerUp().getName() : team2.getRunnerUp().getName();
+                            //this if-statement triggers if the submit scores button was pushed on the final match otherwise it updates the team in the next match
+                            
+                            String third = ""; //get third place team based on scores of runner ups
+                            if(Integer.parseInt(team1.getRunnerUp().getScoreField().getText()) > (Integer.parseInt(team2.getRunnerUp().getScoreField().getText())))
+                            {
+                                third = team1.getRunnerUp().getName();
+                            } else if(Integer.parseInt(team1.getRunnerUp().getScoreField().getText()) < (Integer.parseInt(team2.getRunnerUp().getScoreField().getText())))
+                            {
+                                third = team2.getRunnerUp().getName();
+                            } else { //it is possible to tie here so we display both teams
+                                third = team1.getRunnerUp().getName() + " tied with " + team2.getRunnerUp().getName();
+                            }
+                            
+                            //show alert for winners
                             alert.setTitle("Tournament Over");
                             alert.setHeaderText("We have a winner!");
                             alert.setContentText("First Place: " + team2.getName() + "\nSecond Place: " + team1.getName() + "\nThirdPlace: " +  third);
                             alert.showAndWait();
                         }
                         
+                        //disable score fields for the completed games
                         team1.getScoreField().setDisable(true);
                         team2.getScoreField().setDisable(true);
                     }
                 }
             } catch(Exception err)
             {
+                //catches the exception if non integer scores are given
                 alert.setTitle("Warning");
                 alert.setHeaderText("Warning, non integer input for score");
                 alert.setContentText("Please only choose integer scores.");
@@ -86,6 +118,11 @@ public class Match {
         });
     }
     
+    /**
+     * Creates a match filled with two teams
+     * @param team1Name - one team
+     * @param team2Name - the other team
+     */
     public Match(String team1Name, String team2Name)
     {
         this();
@@ -113,6 +150,11 @@ public class Match {
         }
     }
     
+    /**
+     * gets a team by number
+     * @param number of the team to get
+     * @return the team
+     */
     public Team getTeam(int number)
     {
         if(number == 1)
@@ -126,6 +168,11 @@ public class Match {
         }
     }
     
+    /**
+     * sets the name of a team in the match
+     * @param number - the team number to set
+     * @param s - the string to set it to
+     */
     public void setTeamName(int number, String s)
     {
         if(number == 1)
@@ -140,6 +187,9 @@ public class Match {
         }
     }
     
+    /**
+     * @return returns a reference to this matches submit button
+     */
     public Button getSubmit()
     {
         return submitButton;
